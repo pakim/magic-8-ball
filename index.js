55c6dc11a0dce10d202182e2d5302fbd6d87ac3c
Paul Kim <paukim59@yahoo.com>
2024-04-01 16:50:27 -0700

    Add responses and background

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 let side8 = true;
 const rotateDuration = 3000;
+const responses = [
+  "It is certain",
+  "It is decidedly so",
+  "Without a doubt",
+  "Yes definitely",
+  "You may rely on it",
+  "As I see it, yes",
+  "Most likely",
+  "Outlook good",
+  "Yes",
+  "Signs point to yes",
+  "Reply hazy, try again",
+  "Ask again later",
+  "Better not tell you now",
+  "Cannot predict now",
+  "Concentrate and ask again",
+  "Don't count on it",
+  "My reply is no",
+  "My sources say no",
+  "Outlook not so good",
+  "Very doubtful"
+];
 
 $(".ball").on("click", () => {
   if(!$(".rotate-div").hasClass("disabled")) {
@@ -7,14 +29,20 @@ $(".ball").on("click", () => {
       $(".rotate-div").addClass("active");
       $(".rotate-div").addClass("disabled");
       side8 = false;
+      const currentIndex = Math.floor(Math.random() * 20);
+      $(".message p").text(responses[currentIndex].toUpperCase());
   
       setTimeout(() => {
         $(".rotate-div").removeClass("disabled");
+        $(".message").addClass("active");
+        $(".triangle").addClass("active");
       }, rotateDuration);
     }
     else {
       $(".rotate-div").removeClass("active");
       $(".rotate-div").addClass("disabled");
+      $(".message").removeClass("active");
+      $(".triangle").removeClass("active");
       side8 = true;
   
       setTimeout(() => {
